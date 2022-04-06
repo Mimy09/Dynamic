6b0e98f906382b2c961ca00430524b612b9809e4
Mimy <mimy@grapevine.com.au>
2022-04-06 14:06:51 +1000

    Added headers for DLinkList

---
+++ src/Container/DLinkList.internal.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "DLinkList.h"
+
+struct DLinkList_Node {
+	void* _data;
+	struct DLinkList_Node* _next;
+	struct DLinkList_Node* _prev;
+};
+
+struct DLinkList {
+	DLinkList_Node* _head;
+	DLinkList_Node* _tail;
+};
